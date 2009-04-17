d71a12ce0fc8ad9c096bf01d2c08d4987ba2fa94
tconnor <timocratic@gmail.com>
2009-04-17 16:25:41 -0700

    a little reorg of file structure

--- lib/rack-flash.rb
+++ lib/rack-flash.rb
@@ -1,1 +1,4 @@
-require File.join(File.dirname(__FILE__), *%w[rack flash])
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'rack')
+require 'builder_hack'
+require 'flash'
+require 'flash_hash'
